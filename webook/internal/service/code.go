a8618cb6c2511225313aff00f2d134b7948ab6cc
cjx <1532620642@qq.com>
2024-05-16 22:09:55 +0800

    实现阿里云与腾讯的登录实现1

--- webook/internal/service/code.go
+++ webook/internal/service/code.go
@@ -1,1 +1,30 @@
-package sms
+package service
+
+import (
+	"Clould/webook/internal/service/sms"
+	"context"
+)
+
+type CodeService struct {
+	repo *repository.CodeRepository
+	sms  sms.Service
+}
+
+// 发验证码
+func (svc *CodeService) Send(ctx context.Context,
+	// 区别业务场景
+	biz string,
+	// 验证码 谁来生成
+	phone string) error {
+
+}
+
+func (svc *CodeService) Verify(ctx context.Context,
+	biz string, phone, inputCode string) (bool, error) {
+
+}
+
+func (svc *CodeService) VerifyV1(ctx context.Context,
+	biz string, phone, inputCode string) error {
+
+}
--- webook/internal/service/sms/aliyun/service.go
+++ webook/internal/service/sms/aliyun/service.go
@@ -1,9 +1,68 @@
 package aliyun
 
-import sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+import (
+	"context"
+
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	dysmsapi "github.com/alibabacloud-go/dysmsapi-20170525/v2/client"
+	console "github.com/alibabacloud-go/tea-console/client"
+	util "github.com/alibabacloud-go/tea-utils/service"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/ecodeclub/ekit"
+)
 
 type Service struct {
-	appId     string
-	signature string
-	client    sms.Client
+	appId    *string
+	signName *string
+	client   *dysmsapi.Client
+}
+
+func NewService(client *dysmsapi.Client, appId string, signName string) *Service {
+	return &Service{
+		appId:    &appId,
+		signName: &signName,
+		client:   client,
+	}
+}
+
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *dysmsapi.Client, _err error) {
+	config := &openapi.Config{}
+	config.AccessKeyId = accessKeyId
+	config.AccessKeySecret = accessKeySecret
+	_result = &dysmsapi.Client{}
+	_result, _err = dysmsapi.NewClient(config)
+	return _result, _err
+}
+
+func (s *Service) Send(ctx context.Context, tplId string,
+	args []string, numbers ...string) error {
+	// 1.发送短信
+	sendReq := &dysmsapi.SendSmsRequest{
+		PhoneNumbers:  s.toPtrSlice(numbers)[0],
+		SignName:      s.signName,
+		TemplateCode:  ekit.ToPtr[string](tplId),
+		TemplateParam: s.toPtrSlice(args)[0],
+	}
+	sendResp, _err := s.client.SendSms(sendReq)
+	if _err != nil {
+		return _err
+	}
+
+	code := sendResp.Body.Code
+	if !tea.BoolValue(util.EqualString(code, tea.String("OK"))) {
+		console.Log(tea.String("错误信息: " + tea.StringValue(sendResp.Body.Message)))
+		return _err
+	}
+
+	return nil
+}
+
+// 将字符串切片转换为指针切片
+func (s *Service) toPtrSlice(data []string) []*string {
+	result := make([]*string, len(data))
+	for i, v := range data {
+		value := v
+		result[i] = &value
+	}
+	return result
 }
